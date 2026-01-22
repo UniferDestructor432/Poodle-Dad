919afdf4bc3e74c3f08945e04fb150bdfaaed9a6
Johan Corona González <jcoronagonzalez.06.03.2008@gmail.com>
2026-01-21 23:00:38 -0600

    21.1/01/26

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonImg src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI_2EDD_Caq9eueFaNi7WrB0qErupkm6M6EA&s' style={{height : '20%', justifyContent: 'center', alignContent: 'center', marginTop: '50%'}}></IonImg>
+        <IonImg src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI_2EDD_Caq9eueFaNi7WrB0qErupkm6M6EA&s' style={{height : '20%', justifyContent: 'center', alignContent: 'center', marginTop: '55%'}}></IonImg>
         <IonTitle style={{textAlign: 'center', fontFamily: "Pacifico", fontWeight: 400, }}>Poodle-Master</IonTitle>
         <IonText color='medium' style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>¿Están listos para ser mi nueva familia?</IonText>
         <IonButton routerLink='/main' style={{display: 'flex', justifyContent: 'center', marginRight: '30%', marginLeft: '30%', '--border-radius' : '15px', marginTop: '20%', '--background': 'salmon'}}>WOOF!</IonButton>
